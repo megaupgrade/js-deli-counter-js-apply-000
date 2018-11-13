eb153f29970bef0018701420098c132d91134777
Andy Thach <megaupgrade1@yahoo.com>
2018-11-13 21:47:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-root = true
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
 
-[*]
-end_of_line = lf
-indent_style = space
-insert_final_newline = true
+  const numbersAndNames = []
 
-[*{.java,.py}]
-indent_size = 4
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
 
-[*.{js,json,rb}]
-charset = utf-8
-indent_size = 2
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+};
+
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
